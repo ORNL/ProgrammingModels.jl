faf100f8a468499d8399ece4b9269e527cbba902
William F Godoy <williamfgc@yahoo.com>
2022-10-24 10:57:44 -0400

    Alias for CUDA and AMDGPU
    Need to test

--- JACC.jl/src/JACC.jl
+++ JACC.jl/src/JACC.jl
@@ -1,5 +1,21 @@
 module JACC
 
-include("array.jl")
+export Array
+
+using Requires: @require
+
+function __init__()
+
+
+    @require CUDA = "052768ef-5323-5732-b1bb-66c8b64840ba" begin
+        const Array = CUDA.CuArray{T,N} where {T,N}
+    end
+
+    @require AMDGPU = "21141c5a-9bdb-4563-92ae-f87d6854732e" begin
+        const Array = AMDGPU.ROCArray{T,N} where {T,N}
+    end
+
+end
+
 
 end # module JACC
